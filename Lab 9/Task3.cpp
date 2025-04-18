4ea14c3341b6dbb4ad76dff384086e9f0b90e574
Abdul Majid <mkabdulmajid2006@gmail.com>
2025-04-18 22:40:27 +0500

    Create Task3.cpp

---
+++ Lab 9/Task3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Book.h"
+
+using namespace std;
+
+int main() 
+{
+    Book myBook("1984", "George Orwell", "9780451524935");
+
+    cout << "Book Title : " << myBook.getTitle() << endl;
+    cout << "Author : " << myBook.getAuthor() << endl;
+    cout << "ISBN : " << myBook.getISBN() << endl;
+
+    return 0;
+}
