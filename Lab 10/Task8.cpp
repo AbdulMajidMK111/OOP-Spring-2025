f3af0915a520c9a3ed4a2547675af7b9deddbf7a
Abdul Majid <mkabdulmajid2006@gmail.com>
2025-05-04 22:35:32 +0500

    Create Task8.cpp

---
+++ Lab 10/Task8.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() 
+{
+    {
+        ofstream outFile("config.txt");
+        outFile << "AAAAABBBBBCCCCC";
+        outFile.close();
+    }
+
+    fstream file("config.txt", ios::in | ios::out);
+    if (!file) 
+    {
+        cerr << "Error opening file!" << endl;
+        return 1;
+    }
+
+    file.seekp(5);
+    file << "XXXXX";
+    file.close();
+
+    ifstream inFile("config.txt");
+    string content;
+    getline(inFile, content);
+    cout << "Updated content: " << content << endl;
+    inFile.close();
+
+    return 0;
+}
